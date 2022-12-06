e7dbfc4232a75542b9672b3a8b6e8ec122fae59c
Elmir Khalilov <edik.halilov1999@gmail.com>
2022-12-06 20:16:44 +0400

    Add a list of songs as an external file
    Did it to add readability

---
+++ songs.js
@@ -0,0 +1,44 @@
+const musicList = [
+  {
+    musicCoverFileName: 'cover_asian-kung-fu_1',
+    musicFileName: 'music_asian-kung-fu_1',
+    musicTitle: 'Re:Re:',
+    musicArtist: 'Asian Kung-Fu Generation',
+  },
+  {
+    musicCoverFileName: 'cover_frank-sinatra_1',
+    musicFileName: 'music_frank-sinatra_1',
+    musicTitle: 'My Way',
+    musicArtist: 'Frank Sinatra',
+  },
+  {
+    musicCoverFileName: 'cover_kinokoteikoku_1',
+    musicFileName: 'music_kinokoteikoku_1',
+    musicTitle: 'ロンググッドバイ',
+    musicArtist: 'Kinoko Teikoku',
+  },
+  {
+    musicCoverFileName: 'cover_rosa-walton_1',
+    musicFileName: 'music_rosa-walton_1',
+    musicTitle: 'I Really Want to Stay at Your House',
+    musicArtist: 'Rosa Walton & Hallie Coggins',
+  },
+  {
+    musicCoverFileName: 'cover_sakanaction_1',
+    musicFileName: 'music_sakanaction_1',
+    musicTitle: 'Aoi',
+    musicArtist: 'Sakanaction',
+  },
+  {
+    musicCoverFileName: 'cover_toe_1',
+    musicFileName: 'music_toe_1',
+    musicTitle: 'Goodbye',
+    musicArtist: 'toe',
+  },
+  {
+    musicCoverFileName: 'cover_xxxtenction_1',
+    musicFileName: 'music_xxxtenction_1',
+    musicTitle: 'Changes',
+    musicArtist: 'XXXTenction',
+  },
+];
