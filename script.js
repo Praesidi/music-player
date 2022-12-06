35e647884e712bcd7385202c2af965dbd2f2468e
Elmir Khalilov <edik.halilov1999@gmail.com>
2022-12-06 21:24:15 +0400

    Write a function to update info about a song, fix naming

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
-const music = document.querySelector('audio');
+// const song = document.querySelector('audio');
+const song = document.getElementById ('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
 
-const musicCover = document.querySelector('img');
-const musicArtist = document.getElementById('artist');
-const musicTitle = document.getElementById('title');
+const songCover = document.querySelector('img');
+const songArtist = document.getElementById('artist');
+const songTitle = document.getElementById('title');
 
 let isPlaying = false;
 
-music.volume = 0.01; //add function to change volume
+song.volume = 0.01; //add function to change volume
 
-function playMusic(){
+function playSong(){
   isPlaying = true;
   playBtn.classList.replace('fa-play', 'fa-pause');
   playBtn.setAttribute('title', 'Pause');
-  music.play();
+  song.play();
 }
 
-function pauseMusic(){
+function pauseSong(){
   isPlaying = false;
   playBtn.classList.replace('fa-pause', 'fa-play');
   playBtn.setAttribute('title', 'Play');
-  music.pause();
+  song.pause();
 }
 
-playBtn.addEventListener('click', () => isPlaying ? pauseMusic() : playMusic());
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+//write a fanction to change font-size if title is too long to fit
+function loadSong(songName){
+  songTitle.textContent = songName.songTitle;
+  songArtist.textContent = songName.songArtist;
+  songCover.src = `img/cover_${songName.fileName}.jpeg`;
+  song.src = `music/music_${songName.fileName}.mp3`;
+}
+
+loadSong(songsList[3]);
+
+prevBtn.addEventListener('click', playPrevSong);
+nextBtn.addEventListener('click', playNextSong);
